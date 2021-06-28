0693ab97800a0dcdf228b0a37ba5973b537a830b
Msousags <marcelo.sousa@gerdausummit.com>
2021-06-28 11:54:52 -0300

    Daily commit

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,6 +1,6 @@
-import Expenses from "./components/Expenses";
-
-function App() {
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -22,12 +22,18 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
